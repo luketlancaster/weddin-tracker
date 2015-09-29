bc8e53d025ff911d703c095ecb25b8a62c5ff4df
Luke Lancaster <luketlancaster@gmail.com>
2015-09-29 10:40:47 -0500

    delete functionality for notes

--- js/controllers/person.controller.js
+++ js/controllers/person.controller.js
@@ -2,7 +2,23 @@ angular
   .module('WeddingApp')
   .controller('PersonCtrl', PersonCtrl);
 
-function PersonCtrl($routeParams, DataFactory) {
+function PersonCtrl($scope, $routeParams, DataFactory) {
   var vm = this;
   vm.individual = DataFactory.getFolk($routeParams.id);
+
+  vm.note = '';
+
+  vm.individual.$bindTo($scope, "data");
+
+  vm.saveNote = function() {
+    var attendee = new Firebase('https://weddin.firebaseio.com/attendees/' + $routeParams.id + '/notes');
+    attendee.push(vm.note);
+    vm.note = '';
+  }
+
+  vm.remove = function(note) {
+    delete $scope.data.notes[note];
+  }
+
+//$loaded
 }
