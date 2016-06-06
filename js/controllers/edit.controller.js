f16ff28ac89ea46b18500ad3c9b7addc837a1cd0
Luke Lancaster <luketlancaster@gmail.com>
2016-06-06 17:23:46 -0500

    edit person works, small styling changes

--- js/controllers/edit.controller.js
+++ js/controllers/edit.controller.js
@@ -2,8 +2,20 @@ angular
   .module('WeddingApp')
   .controller('EditCtrl', EditCtrl);
 
-function EditCtrl($scope, $routeParams, DataFactory) {
+function EditCtrl($scope, $routeParams, DataFactory, $location) {
   var vm = this;
   vm.individual = DataFactory.getFolk($routeParams.id);
+
+
+  vm.savePerson = function() {
+    vm.individual
+        .$save()
+        .then(function(ref) {
+            $location.path('/person/' + vm.individual.$id);
+        }, function(error) {
+            console.log("It didn't save!!!\n");
+            console.log(error);
+        });
+  };
 
 }
